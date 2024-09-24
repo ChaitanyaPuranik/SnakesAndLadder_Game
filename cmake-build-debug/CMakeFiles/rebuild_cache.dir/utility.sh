7de3d1566481987646e44ef61aea9ca15509cf84
Chaitanya <puranikc@shernet.sheridancollege.ca>
2024-09-24 12:48:58 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd /cygdrive/d/Programming/C/SnakesAndLadder-Game/cmake-build-debug
+/cygdrive/c/Users/DEVENDRA/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
